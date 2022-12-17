7ed0276b8bc2d7e6462ee6f83bda0ca8aa11c147
Shahbaz786920 <shahbazlhan42@gmail.com>
2022-12-17 15:21:58 +0530

    second commit

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const server=express();
 const fs = require('fs');
 const user = require('./user.json');
+const response = require('./response.json');
+
 server.post('/student/add',(req,res)=>{
     res.status(200)
     res.send(JSON.stringify(user));
 })
 server.get('/student/getDetails',(req,res)=>{
-    res.status(200).send(JSON.stringify(user));
+    res.status(200).send(JSON.stringify(response));
 })
 
 const data = fs.readFileSync("user.json","utf-8");
